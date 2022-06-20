50b1879336a6c50bfbbf569f32899865b5ad8568
unknown <sysadmin@BGV_NITINC.WALSONSVERIFY.COM>
2022-06-21 00:50:59 +0530

    first commit with all POM practice code

--- src/main/java/com/qa/opencart/pages/AccAdvPage.java
+++ src/main/java/com/qa/opencart/pages/AccAdvPage.java
@@ -3,7 +3,7 @@ package com.qa.opencart.pages;
 public class AccAdvPage {
 
 	public static void main(String[] args) {
-		System.out.println("acc adv page");
+		System.out.println("acc adv page new");
 
 	}
 
