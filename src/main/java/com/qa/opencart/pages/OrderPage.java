0d607465de8f1451ea851d305c2ac8da2136d39c
Nitin <nitinchamp6@gmail.com>
2022-06-26 01:33:40 +0530

    added order page after resolving merge conflict

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -6,10 +6,8 @@ public class OrderPage {
 	
 	
 	public void makeOrder() {
-<<<<<<< HEAD
+
 		System.out.println("order id : 123456");
-=======
-		System.out.println("order id :12345");
->>>>>>> 7ec3bda1d18dcdc730fb9129a10c616f1e38f7ab
+
 	}
 }
